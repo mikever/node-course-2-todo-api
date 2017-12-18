c8dfa868600a8e3edfbb533d94e1af011f180665
Michael <mveronie@gmail.com>
2017-12-18 05:06:58 -0500

    Updated keys for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,7 +4,7 @@ let key = require('./keys/keys');
 
 let db = {
   localhost: 'mongodb://localhost:27017/TodoApp',
-  mlab: `mongodb://mikever:iyw4pp4w@M@ds161016.mlab.com:61016/mv-node-todo-api`
+  mlab: `mongodb://${key.mlabUser}:${key.mlabPass}@ds161016.mlab.com:61016/mv-node-todo-api`
 }
 
 mongoose.Promise = global.Promise;
