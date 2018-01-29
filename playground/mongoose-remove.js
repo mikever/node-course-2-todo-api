e66274f9507d1174c9ea248483e3777f68347f7d
Michael <mveronie@gmail.com>
2018-01-29 05:45:14 -0500

    Refactoring DELETE /todos/:id route

--- playground/mongoose-remove.js
+++ playground/mongoose-remove.js
@@ -1,4 +1,5 @@
 const {ObjectID} = require('mongodb');
+
 const {mongoose} = require('./../server/db/mongoose');
 const {Todo} = require('./../server/models/todo');
 const {User} = require('./../server/models/user');
@@ -7,13 +8,13 @@ const {User} = require('./../server/models/user');
 //   console.log(result);
 // });
 
-// Todo.findOneAndRemove()
+// Todo.findOneAndRemove({})
 // Todo.findByIdAndRemove()
 
-todo.findOneAndRemove({_id: 'someidhere'}).then((todo) => {
-  console.log(todo);
-});
+// Todo.findOneAndRemove({ _id: '5a6ef431b2e32b8be5cecfc8'}).then((todo) => {
+//   console.log(todo);
+// });
 
-Todo.findByIdAndRemove('asd').then((todo) => {
+Todo.findByIdAndRemove('5a6ef431b2e32b8be5cecfc8').then((todo) => {
   console.log(todo);
 });
