b5ea278143e64e6ff696f6ad0d8286983b7a13f2
Michael <mveronie@gmail.com>
2018-01-31 04:53:06 -0500

    Refactoring of PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,8 +1,9 @@
 // import { ObjectID } from '../../../../.cache/typescript/2.6/node_modules/@types/bson';
 
+const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
-let {ObjectID} = require('mongodb');
+const {ObjectID} = require('mongodb');
 
 let {mongoose} = require('./db/mongoose');
 let {Todo} = require('./models/todo');
@@ -68,6 +69,32 @@ app.delete('/todos/:id', (req, res) => {
     res.status(400).send();
   });
 });
+
+app.patch('/todos/:id', (req, res) => {
+  let id = req.params.id;
+  let body = _.pick(req.body, ['text', 'completed']);
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+  
+  Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  })
+})
 
 app.listen(port, () => {
   console.log(`Started on port ${port}`);
